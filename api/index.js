91b1ad045747b772397c1bfa14bf4a2a5ded3367
Nicollas Matsuo <144185296+matsuonicollas@users.noreply.github.com>
2025-05-04 17:22:02 +0000

    crud-mongo-feito

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import db from './database/configdb.js';
+import User from './models/User.js';
+import userRoute from './routes/userroute.js';
+import exemploeroute from './routes/exampleroute.js';
+
+dotenv.config();
+db.connect();
+
+const app = express();
+
+app.use(express.json());
+app.use("/user", userRoute);
+app.use("/protected", exemploeroute);
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello World!' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}/`);
+});
